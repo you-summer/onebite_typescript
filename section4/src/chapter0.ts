2cfe49b0d4d416a82d9d9c884b6f5137d242741e
조연정 <jojoor201@gmail.com>
2025-09-01 14:40:46 +0900

    day7

---
+++ section4/src/chapter0.ts
@@ -0,0 +1,42 @@
+/**
+ * 함수 타입 정의
+ */
+
+// 함수를 설명하는 가장 좋은 방법
+// 어떤 매개변수를 받고, 어떤 결과값을 반환하는지 이야기
+// 어떤 [타입의] 매개변수를 받고, 어떤 [타입의] 결과값을 반환하는지 이야기
+function func(a: number, b: number) {
+  return a + b;
+}
+
+/**
+ * 화살표 함수의 타입을 정의하는 방법
+ */
+
+const add = (a: number, b: number) => a + b;
+
+/**
+ * 함수의 매개변수
+ */
+
+function introduce(name = "조연정", age: number, tall?: number) {
+  console.log(`name : ${name}`);
+  if (typeof tall === "number") {
+    console.log(`tall : ${tall + 10}`);
+  }
+}
+
+introduce("조연정", 33, 168);
+
+introduce("조연정", 33);
+
+function getSum(...rest: number[]) {
+  let sum = 0;
+  rest.forEach((it) => {
+    return (sum += it);
+  });
+  return sum;
+}
+
+getSum(1, 2, 3); //6;
+getSum(1, 2, 3, 4, 5); //15
---
+++ section4/src/chapter1.ts
@@ -0,0 +1,24 @@
+/**
+ * 함수 타입 표현식
+ */
+
+type Operation = (a: number, b: number) => number;
+
+const add: (a: number, b: number) => number = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const multiply: Operation = (a, b) => a * b;
+const divide: Operation = (a, b) => a / b;
+
+/**
+ * 호출 시그니처
+ * (콜 시그니처)
+ */
+
+type Operation2 = {
+  (a: number, b: number): number;
+};
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const multiply2: Operation2 = (a, b) => a * b;
+const divide2: Operation2 = (a, b) => a / b;
